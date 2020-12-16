85002504505433f8fcb58be68682604fc647c644
exblematique <yoann.chappaz@outlook.com>
2020-12-16 13:27:28 +0100

    Add index

--- index.php
+++ index.php
@@ -3,8 +3,28 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
 <body>
-
-
+<header>
+    <a href="index.php">Inicio</a>
+    <a href="search.php">Buscar</a>
+    <a href="cart.php">Cesta>/a>
+    <?php // Change the link if user is connected or not
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){?>
+        <a href="logout.php">Cerrar sesión</a>
+    <?php } else {?>
+        <a href="login.php">Initiar sesión</a>
+    <?php } ?>
+</header>
+<h1>Bienvenido a nuestro nuevo sitio web de comercio electrónico.</h1>
+<h1>No dudes en ir a las diferentes pestañas en la parte superior de la página.</h1>
 </body>
 </html>
