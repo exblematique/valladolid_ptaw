ad4a1ac3d4c7699c1a647c2fa1c940272af89db3
exblematique <yoann.chappaz@outlook.com>
2020-12-16 13:31:24 +0100

    Improve index

--- index.php
+++ index.php
@@ -9,19 +9,20 @@ session_start();
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         body{ font: 14px sans-serif; }
+        header{display: flex;}
         .wrapper{ width: 350px; padding: 20px; }
     </style>
 </head>
 <body>
 <header>
-    <a href="index.php">Inicio</a>
-    <a href="search.php">Buscar</a>
-    <a href="cart.php">Cesta>/a>
+    <a class="button" href="index.php">Inicio</a>
+    <a class="button" href="search.php">Buscar</a>
+    <a class="button" href="cart.php">Cesta</a>
     <?php // Change the link if user is connected or not
     if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){?>
-        <a href="logout.php">Cerrar sesión</a>
+        <a class="button" href="logout.php">Cerrar sesión</a>
     <?php } else {?>
-        <a href="login.php">Initiar sesión</a>
+        <a class="button" href="login.php">Initiar sesión</a>
     <?php } ?>
 </header>
 <h1>Bienvenido a nuestro nuevo sitio web de comercio electrónico.</h1>
