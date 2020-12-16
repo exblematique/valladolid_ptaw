a7a53376bbb4259a950105bd744d3f92ba9b4528
exblematique <yoann.chappaz@outlook.com>
2020-12-16 13:10:19 +0100

    update database

--- create.sql
+++ create.sql
@@ -1,6 +1,16 @@
 CREATE TABLE users (
-   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-   username VARCHAR(50) NOT NULL UNIQUE,
-   password VARCHAR(255) NOT NULL,
-   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+   id           INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   username     VARCHAR(50) NOT NULL UNIQUE,
+   password     VARCHAR(255) NOT NULL,
+   created_at   DATETIME DEFAULT CURRENT_TIMESTAMP,
+   isAdmin      BOOLEAN DEFAULT FALSE
 );
+
+CREATE TABLE products (
+    id          INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL UNIQUE,
+    category    VARCHAR(50)  NOT NULL,
+    brand       VARCHAR(50)  NOT NULL,
+    color       VARCHAR(50),
+    price       DECIMAL(9,2) NOT NULL
+)
