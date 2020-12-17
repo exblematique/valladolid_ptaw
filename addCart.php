2da0600882cdbb6c7ca0debb50cff5f45504abd3
exblematique <yoann.chappaz@outlook.com>
2020-12-17 01:20:28 +0100

    Add degub symbol

--- addCart.php
+++ addCart.php
@@ -1,15 +1,22 @@
 <?php
 session_start();
+
+// Contains the result of this function
+$result = array();
+
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $result["id"] = $_POST["id"];
     // Check if "id" and "quantity" are not empty and are numbers
     if (isset($_POST["id"]) and is_numeric($_POST["id"]) and isset($_POST["quantity"]) and is_numeric($_POST["quantity"])){
         $id = intval($_POST["id"]);
         $quantity = intval($_POST["quantity"]);
 
         // If array doesn't exist, create it
-        if (!isset($_SESSION['cart']))
+        if (!isset($_SESSION['cart'])) {
             $_SESSION['cart'] = array();
+            $result["create"] = true;
+        }
 
         // If element is not in cart add it
         // Else add with the current quantity
@@ -20,7 +27,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
         // Cart cannot be negative or null
         // Remove element if it is the case
-        if ($_SESSION['cart'][$id] <= 0)
+        if ($_SESSION['cart'][$id] <= 0){
             unset($_SESSION['cart'][$id]);
+            $result['remove'] = true;
+        }
+
+        $result['cart'] = $_SESSION['cart'];
+        echo json_encode($result);
     }
 }
