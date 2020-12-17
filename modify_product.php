672dd066137f6bfce783ff72f656f2076939f068
Raphaël Sauret <rsauret.ir2021@esaip.org>
2020-12-17 18:36:11 +0100

    final2

--- modify_product.php
+++ modify_product.php
@@ -76,11 +76,11 @@ mysqli_close($link);
         </tr>
         <tr>
             <td id="id"><?php echo json_encode($product['id']) ?></td>
-            <td id="name"><input type="text" name="updated_name" id="updated_name" placeholder="<?php echo json_encode($product['name']) ?>"></td>
-            <td id="category"><input type="text" name="updated_category" id="updated_category" placeholder="<?php echo json_encode($product['category']) ?>"></td>
-            <td id="brand"><input type="text" name="updated_brand" id="updated_brand" placeholder="<?php echo json_encode($product['brand']) ?>"></td>
-            <td id="color"><input type="text" name="updated_color" id="updated_color" placeholder="<?php echo json_encode($product['color']) ?>"></td>
-            <td id="price"><input type="text" name="updated_price" id="updated_price" placeholder="<?php echo json_encode($product['price']) ?>"></td>
+            <td id="name"><input type="text" name="updated_name" id="updated_name" value="<?php echo json_encode($product['name']) ?>"></td>
+            <td id="category"><input type="text" name="updated_category" id="updated_category" value="<?php echo json_encode($product['category']) ?>"></td>
+            <td id="brand"><input type="text" name="updated_brand" id="updated_brand" value="<?php echo json_encode($product['brand']) ?>"></td>
+            <td id="color"><input type="text" name="updated_color" id="updated_color" value="<?php echo json_encode($product['color']) ?>"></td>
+            <td id="price"><input type="text" name="updated_price" id="updated_price" value="<?php echo json_encode($product['price']) ?>"></td>
         </tr>
     </table>
     <input type="hidden" name="update_product" id="update_product" value="true"><br/>
