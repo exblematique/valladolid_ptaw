50fee9b72b4c6f8dd0edb22b3bd1c8652083537a
exblematique <yoann.chappaz@outlook.com>
2020-12-17 15:45:39 +0100

    Active debug

--- order.php
+++ order.php
@@ -4,7 +4,7 @@ session_start();
 // Define variables
 $error = "";
 $order = "";
-$debug = false;
+$debug = true;
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(!isset($_SESSION["loggedin"]) or $_SESSION["loggedin"] !== true){
