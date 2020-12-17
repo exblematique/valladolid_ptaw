a9d040bd433448cfbd4d58e4f6d956974ff7a7db
exblematique <yoann.chappaz@outlook.com>
2020-12-17 15:59:06 +0100

    Test 2

--- order.php
+++ order.php
@@ -25,11 +25,11 @@ $cart = array();
 
 // Check if the order exist and belongs to user
 if (!empty($error)) {
-    $sql = "SELECT id, id_user, created_at, delivery_date FROM orders WHERE id = ?";
+    $sql = "SELECT id, id_user, created_at, delivery_date FROM orders WHERE id = $orderId";
 
     if ($stmt = mysqli_prepare($link, $sql)) {
         // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "s", $_GET["id"]);
+        //mysqli_stmt_bind_param($stmt, "s", $orderId);
 
         if (mysqli_stmt_execute($stmt)) {
             mysqli_stmt_store_result($stmt);
